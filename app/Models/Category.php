9df9dc6a802a91f03dc04ab5dd2d28441b04f1d0
ltvilla <tiago_villa1@hotmail.com>
2021-05-15 07:41:14 -0300

    up repository

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
+
+class Category extends Model
+{
+    use SoftDeletes, \App\Models\Traits\Uuid;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'is_active'
+    ];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
+}
+
