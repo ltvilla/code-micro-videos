db5ecf1567658dc4c0d0c6c67a503404fc992651
Tiago <tiago_villa1@hotmail.com>
2021-08-31 06:41:37 -0300

    change project file

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+cd backend
 cp .env.example .env
 cp .env.testing.example .env.testing
 chown -R www-data:www-data .
